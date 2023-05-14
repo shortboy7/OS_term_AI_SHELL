55816ec809f177cd2c1bdcecc904e8ae5df0e916
shortboy <shortboy7>
2023-05-14 20:08:45 +0900

    add export

--- srcs/main.c
+++ srcs/main.c
@@ -54,7 +54,7 @@ int main() {
 
         // Execute builtin
         if (strcmp(arguments[0], "cd") == 0 || strcmp(arguments[0], "history") == 0
-            || strcmp(arguments[0], "exit") == 0) {
+            || strcmp(arguments[0], "exit") == 0 || strcmp(arguments[0], "export") == 0) {
             if (strcmp(arguments[0], "cd") == 0)
                 changeDirectory(arguments);
             else if (strcmp(arguments[0], "history") == 0)
@@ -64,6 +64,12 @@ int main() {
                     free(arguments[i]);
                 }
                 break;
+            }
+            else if (strcmp(arguments[0], "export") == 0) {
+            if (arguments[1] != NULL && arguments[2] != NULL)
+                setEnvironmentVariable(arguments[1], arguments[2]);
+            else
+                printf("Invalid arguments for export command.\n");
             }
             continue;
         } else {
@@ -79,3 +85,11 @@ int main() {
     freeHistory(history);
     return 0;
 }
+
+// 1. built-in 함수와 shell command, 외부 프로그램을 모두 실행시킬 수 있어야 함.
+// 2. 사용자가 사용했던 명령어들을 기억하고, 다시 칠 수 있는 기능을 history같은 명령어나 단단축키로 사용할 수 있어야 함.
+// 3. 탭으로 파일 명이나 디렉토리, 명령어들을 자동완성 할 수 있어야 함.
+// 4. 리다이렉션, 파이프처리를 수행할 수 있어야 함.
+// 5. 환경 변수를 가져오거나 변경할 수 있어야 함.
+// 6. 작업을 background나 foreground에서 처리할 수 있어야 함.
+// 7. 파일이나 디렉터리 작업을 수행할 수 있어야 함.
